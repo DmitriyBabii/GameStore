e6579766aaa77f146f3c493db870e263078e9c35
DmitriyBabii <v.babiy75@gmail.com>
2022-06-11 23:27:31 +0300

    Some cool changes

--- src/main/java/models/enums/ERating.java
+++ src/main/java/models/enums/ERating.java
@@ -1,19 +1,29 @@
 package models.enums;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 
 @Getter
-@AllArgsConstructor
 public enum ERating {
-    id_user_fk("CREATE TABLE IF NOT EXISTS `gameshop`.`rating` (\n"
-            + "`id_user_fk` VARCHAR(36),\n"),
-    id_product_fk("`id_product_fk` VARCHAR(36),\n"),
-    review("`review` VARCHAR(255) NOT NULL,\n"),
-    review_date("`review_date` DATE NOT NULL,\n"
+    id_user_fk("CREATE TABLE IF NOT EXISTS gameshop.rating (\n"
+            + "id_user_fk", MySQLType.ID, MySQLAttribute.NOT_NULL),
+    id_product_fk("id_product_fk", MySQLType.ID, MySQLAttribute.NOT_NULL),
+    review("review", MySQLType.LONG_STRING, MySQLAttribute.NOT_NULL),
+    review_date("review_date " + MySQLType.DATE + MySQLAttribute.NOT_NULL + ",\n"
             + "FOREIGN KEY (id_user_fk)  REFERENCES user (id_user),\n"
             + "FOREIGN KEY (id_product_fk)  REFERENCES product (id_product));");
 
     private final String query;
+
+    ERating(String query) {
+        this.query = query;
+    }
+
+    ERating(String query, MySQLType type, MySQLAttribute... attribute) {
+        StringBuilder str = new StringBuilder(query + type);
+        for (MySQLAttribute mySQLAttribute : attribute) {
+            str.append(mySQLAttribute);
+        }
+        this.query = str.append(",\n").toString();
+    }
 }
